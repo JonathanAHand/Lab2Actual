4e16799edde5a85fafa91473cfdd7b46f14cd0c7
Jonathan A Hand <jhand1@my.westga.edu>
2024-08-27 16:39:14 -0400

    Added code folder to repo
    Moved code from repo that was improperly stored

---
+++ code/Lab 1/src/edu/westga/cs1302/lab1/model/BillItem.java
@@ -0,0 +1,66 @@
+package edu.westga.cs1302.lab1.model;
+
+/** Stores information for a single item on a bill.
+ * 
+ * @author CS 1302
+ * @version Fall 2024
+ */
+public class BillItem {
+	private String name;
+	private double amount;
+	/** Creates a new bill item with the provided name and amount.
+	 * 
+	 * @precondition name != null &&
+	 * 				 amount > 0
+	 * @postcondition getName() == name &&
+	 * 				  getAmount() == amount
+	 * 
+	 * @param name the name of the bill item
+	 * @param amount the amount (cost) of the bill item
+	 */
+	public BillItem(String name, double amount, String item) {
+		if (name == null) {
+			throw new IllegalArgumentException("name must not be null.");
+		}
+		if (amount <= 0) {
+			throw new IllegalArgumentException("amount must be positive, but was " + amount);
+		}
+		this.name = name;
+		this.amount = amount;
+	}
+	
+
+	/** Return the name for the bill item
+	 * 
+	 * @precondition none
+	 * @postcondition none
+	 * 
+	 * @return the name for the bill item
+	 */
+	public String getName() {
+		return this.name;
+	}
+	
+
+	/** Return the amount for the bill item
+	 * 
+	 * @precondition none
+	 * @postcondition none
+	 * 
+	 * @return the amount for the bill item
+	 */
+	public double getAmount() {
+		return this.amount;
+	}
+	
+	/** Return the item for the bill
+	 * 
+	 * @precondition none
+	 * @postcondition none
+	 * 
+	 * @return the item for the bill
+	 */
+	public String getItem() {
+		return this.getItem();
+	}
+}
---
+++ code/Lab 1/src/edu/westga/cs1302/lab1/view/BillTextGenerator.java
@@ -0,0 +1,51 @@
+package edu.westga.cs1302.lab1.view;
+
+import edu.westga.cs1302.lab1.model.Bill;
+import edu.westga.cs1302.lab1.model.BillItem;
+
+/**
+ * Generates text for the bill.
+ * 
+ * @author Jonathan Hand
+ * @version 1.0
+ * 
+ */
+
+public class BillTextGenerator {
+
+	private BillItem[] items;
+
+	/**
+	 * Generates the actual text for the bill
+	 * 
+	 * @precondition bill != null
+	 * @postcondition
+	 * @param bill the bill text is being generated for
+	 * 
+	 * @return A string with the list of items on the bill and the total
+	 */
+
+	public String generateBillText(Bill bill) {
+		if (bill == null) {
+			throw new IllegalArgumentException("Invalid Input: Bill must not be null.");
+		}
+		StringBuilder billText = new StringBuilder();
+		billText.append("ITEMS").append(System.lineSeparator());
+		double subTotal = 0.0;
+		for (BillItem item : this.items) {
+			billText.append(item.getName()).append(" - ").append(item.getAmount()).append(System.lineSeparator());
+			subTotal += item.getAmount();
+		}
+
+		billText.append(System.lineSeparator());
+		billText.append("Subtotal: $").append(subTotal).append(System.lineSeparator());
+		double billTax = subTotal * 0.07;
+		double billTip = subTotal * 0.2;
+		billText.append("TAX: $").append(billTax).append(System.lineSeparator());
+		billText.append("TIP: $").append(billTip).append(System.lineSeparator());
+		billText.append("Total: $").append(subTotal + billTax + billTip);
+
+		return billText.toString();
+	}
+
+}
